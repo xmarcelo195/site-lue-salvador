6a40767093444da47372646cca5e1ee266b2a65a
Marcelo Silva <xmarcelo195@gmail.com>
2020-08-17 16:31:30 -0300

    menu-01

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function clickMenu() {
+  var items = document.querySelector("#ul_lista")
+  var menu = document.querySelector("#menu_cel")
+  menu.classList.toggle("clicked")
+  items.classList.toggle("clickedUl")
+  }
